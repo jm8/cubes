d86b123bb55e9b4243c5092e4cbbed27f7383836
Joshua Sims <jm8hjkl@gmail.com>
2025-05-01 18:36:13 -0400

    wip

--- src/main.ts
+++ src/main.ts
@@ -181,74 +181,68 @@ window.addEventListener("load", () => {
     position: vec3.fromValues(0, 0, 0),
     velocity: vec3.fromValues(0, 0, 0),
     rotation: vec3.fromValues(0, Math.PI / 8, 0),
-    rotationalVelocity: vec3.fromValues(0, 0, 0),
+    rotationalVelocity: vec3.fromValues(0, .15, 0),
     color: new paper.Color("#ffffff"),
     explosion: .5,
-    scale: 1,
+    scale: 1.5,
   });
+
+  function rand(a: number, b: number): number {
+    return a + (b - a) * Math.random();
+  }
 
   function randomVector(magnitude: number): vec3 {
     // https://math.stackexchange.com/questions/44689/how-to-find-a-random-axis-or-unit-vector-in-3d
-    const theta = Math.random() * 2 * Math.PI;
-    const z = -1 + Math.random() * 2;
+    const theta = rand(0, 2 * Math.PI);
+    const z = rand(-1, 1);
     const a = Math.sqrt(1 - z * z);
     const x = a * Math.cos(theta);
     const y = a * Math.sin(theta);
     return vec3.scale(vec3.create(), vec3.fromValues(x, y, z), magnitude);
   }
 
-  const cubes = [
+  function randomCube(x: number) {
+    return new DoubledCube({
+      color: Math.random() > .5 ? new paper.Color("#ffffff") : new paper.Color("#aa0000"),
+      explosion: 0,
+      position: vec3.fromValues(x, rand(-15, 0), rand(15, 15)),
+      rotation: randomVector(1),
+      rotationalVelocity: randomVector(.3),
+      scale: 1,
+      velocity: vec3.fromValues(rand(.4, 1), 0, 0),
+    })
+  }
+
+  let cubes = [
     cube1,
-    new DoubledCube({
-      position: vec3.fromValues(2, 0, 10),
-      velocity: vec3.fromValues(1, 0, 0),
-      rotation: vec3.fromValues(0, 0, 0),
-      rotationalVelocity: randomVector(.3),
-      color: new paper.Color("#ffffff"),
-      explosion: 0,
-      scale: 1,
-    }),
-    new DoubledCube({
-      position: vec3.fromValues(-4, 1, 15),
-      velocity: vec3.fromValues(1, 0, 0),
-      rotation: randomVector(1),
-      rotationalVelocity: randomVector(.3),
-      color: new paper.Color("#ffffff"),
-      explosion: 0,
-      scale: 1,
-    }),
-    new DoubledCube({
-      position: vec3.fromValues(-8, -8, 15),
-      velocity: vec3.fromValues(1, 0, 0),
-      rotation: randomVector(1),
-      rotationalVelocity: randomVector(.3),
-      color: new paper.Color("#ffffff"),
-      explosion: 0,
-      scale: 1,
-    }),
-    new DoubledCube({
-      position: vec3.fromValues(-3, -10, 15),
-      velocity: vec3.fromValues(1, 0, 0),
-      rotation: randomVector(1),
-      rotationalVelocity: randomVector(.3),
-      color: new paper.Color("#ffffff"),
-      explosion: 0,
-      scale: 1,
-    }),
   ]
+
+
+  for (let x = -20; x < 20; x += 6) {
+    cubes.push(randomCube(x));
+  }
+
+  let initialNumberOfCubes = cubes.length;
 
   let time = 0;
   paper.view.onFrame = ({ delta }: { delta: number }) => {
-    time += delta;
-    while (time > 2) {
-      time -= 2;
-    }
-    if (time < 1) {
-      cube1.explosion = 0.5 * time;
-    } else {
-      cube1.explosion = 0.5 * (1 - (time - 1));
-    }
+    // time += delta;
+    // while (time > 2) {
+    //   time -= 2;
+    // }
+    // if (time < 1) {
+    //   cube1.explosion = 0.5 * time;
+    // } else {
+    //   cube1.explosion = 0.5 * (1 - (time - 1));
+    // }
 
-    cubes.forEach(cube => cube.update(delta));
+    cubes = cubes.filter(cube => {
+      cube.update(delta);
+      return cube.position[0] < 20;
+    });
+
+    while (cubes.length < initialNumberOfCubes) {
+      cubes.push(randomCube(-20));
+    }
   };
 });
