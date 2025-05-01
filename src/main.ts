7148b9a72aa035d9a09b009025c925e03091184c
Joshua Sims <jm8hjkl@gmail.com>
2025-05-01 16:34:19 -0400

    temp

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,14 @@ let PROJECTION_MATRIX: mat4;
 
 
 const CUBE_POINTS = [
-  vec3.fromValues(-.5, -.5, -.5), // 0
-  vec3.fromValues(.5, -.5, -.5), // 1
-  vec3.fromValues(.5, .5, -.5), // 2
-  vec3.fromValues(-.5, .5, -.5), // 3
-  vec3.fromValues(-.5, -.5, .5), // 4
-  vec3.fromValues(.5, -.5, .5), // 5
-  vec3.fromValues(.5, .5, .5), // 6
-  vec3.fromValues(-.5, .5, .5), // 7
+  vec3.fromValues(-1, -1, -1), // 0
+  vec3.fromValues(1, -1, -1), // 1
+  vec3.fromValues(1, 1, -1), // 2
+  vec3.fromValues(-1, 1, -1), // 3
+  vec3.fromValues(-1, -1, 1), // 4
+  vec3.fromValues(1, -1, 1), // 5
+  vec3.fromValues(1, 1, 1), // 6
+  vec3.fromValues(-1, 1, 1), // 7
 ];
 
 const CUBE_FACES = [
@@ -28,23 +28,41 @@ const CUBE_FACES = [
   [1, 2, 6, 5],
 ];
 
+const CUBE_NORMALS = [
+  vec3.fromValues(0, 0, -1),
+  vec3.fromValues(0, 0, 1),
+  vec3.fromValues(0, -1, 0),
+  vec3.fromValues(0, 1, 0),
+  vec3.fromValues(-1, 0, 0),
+  vec3.fromValues(1, 0, 0),
+];
+
 class Cube {
   rotation: vec3;
   position: vec3;
   faces: paper.Path[];
+  scale: number;
+  color: paper.Color;
+  offset: vec2;
+  explosion: number;
 
-  constructor(rotation: vec3, position: vec3) {
+  constructor(rotation: vec3, position: vec3, scale: number, color: paper.Color, offset: vec2, explosion: number) {
     this.rotation = vec3.copy(vec3.create(), rotation);
     this.position = vec3.copy(vec3.create(), position);
+    this.scale = scale;
+    this.color = color;
+    this.offset = offset;
+    this.explosion = explosion;
+    
 
     const points = this.computePoints().map(([x, y]) => {
       return new paper.Point(x, y);
     });
     this.faces = CUBE_FACES.map(face => {
       const path = new paper.Path(face.map(x => points[x]));
-      path.strokeColor = new paper.Color('white');
+      path.strokeColor = this.color;
       path.closed = true;
-      path.strokeWidth = 5;
+      path.strokeWidth = 2;
       path.strokeJoin = 'bevel';
       return path;
     });
@@ -53,14 +71,16 @@ class Cube {
   computePoints(): vec2[] {
     const origin = vec3.create();
     return CUBE_POINTS.map(point => {
-      const transformed = vec3.create();
-      vec3.add(transformed, point, this.position);
+      const transformed = vec3.copy(vec3.create(), point);
       vec3.rotateX(transformed, transformed, origin, this.rotation[0]);
       vec3.rotateY(transformed, transformed, origin, this.rotation[1]);
       vec3.rotateZ(transformed, transformed, origin, this.rotation[2]);
-      vec3.scale(transformed, transformed, 30);
+      vec3.scale(transformed, transformed, this.scale * 30);
+      vec3.scaleAndAdd(transformed, transformed, this.position, 30);
       vec3.transformMat4(transformed, transformed, PROJECTION_MATRIX);
-      return vec2.fromValues(transformed[0], transformed[1]);
+      const result = vec2.fromValues(transformed[0], transformed[1]);
+      vec2.add(result, result, this.offset);
+      return result;
     });
   }
 
@@ -76,6 +96,33 @@ class Cube {
   }
 }
 
+class DoubledCube {
+  rotation: vec3;
+  position: vec3;
+  scale: number;
+  color: paper.Color;
+  offset: vec2;
+
+  cube1: Cube;
+  cube2: Cube;
+
+  constructor(rotation: vec3, position: vec3, scale: number, color: paper.Color, offset: vec2) {
+    this.rotation = vec3.copy(vec3.create(), rotation);
+    this.position = vec3.copy(vec3.create(), position);
+    this.scale = scale;
+    this.color = color;
+    this.offset = offset;
+    this.cube1 = new Cube(rotation, position, scale, color, offset);
+    this.cube2 = new Cube(rotation, position, scale, color, vec2.add(vec2.create(), offset, [5, 5]));
+  }
+
+  update(dt: number) {
+    this.cube1.update(dt);
+    this.cube2.update(dt);
+  }
+}
+
+
 window.addEventListener('load', () => {
   const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
   paper.setup(canvas);
@@ -86,9 +133,9 @@ window.addEventListener('load', () => {
     paper.view.bounds.width / 2, paper.view.bounds.height / 2, 0, 0, // fourth column 
   );
 
-  const cube = new Cube(vec3.fromValues(Math.PI / 2, Math.PI / 2, 0), vec3.fromValues(0, 0, 0));
-  paper.view.onFrame = ({delta}: {delta: number}) => {
-    cube.update(delta);
+  const cube1 = new DoubledCube(vec3.fromValues(Math.PI / 2, Math.PI / 2, 0), vec3.fromValues(0, 0, 0), 1, new paper.Color("#ffffff"), vec2.fromValues(0, 0));
+  paper.view.onFrame = ({ delta }: { delta: number }) => {
+    cube1.update(delta);
   }
 });
 
